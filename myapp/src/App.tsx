8cb1f55659dbfd43f331e2b073721be09c8c512a
k-oyun <dhdbs001208@naver.com>
2024-02-19 20:54:48 +0900

    Header 2

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./Routes/Home"; // 예시: Home 컴포넌트의 파일 경로에 따라 변경되어야 함
-import Tv from "./Routes//Tv"; // 예시: Tv 컴포넌트의 파일 경로에 따라 변경되어야 함
-import Search from "./Routes//Search"; // 예시: Search 컴포넌트의 파일 경로에 따라 변경되어야 함
-import Header from "./Routes/Components/Header";
+import Tv from "./Routes/Tv"; // 예시: Tv 컴포넌트의 파일 경로에 따라 변경되어야 함
+import Search from "./Routes/Search"; // 예시: Search 컴포넌트의 파일 경로에 따라 변경되어야 함
+import Header from "./Components/Header";
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
         <Route path="/tv">
           <Tv />
         </Route>
         <Route path="/search">
           <Search />
+        </Route>
+        {/* 맨 처음경로를 마지막에 두어야함 */}
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </Router>
