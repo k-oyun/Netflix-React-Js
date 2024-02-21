1d801bcefe26dd1e471bd1db25660105927a0fe2
k-oyun <dhdbs001208@naver.com>
2024-02-21 16:15:57 +0900

    Done for modal

--- myapp/src/Routes/Home.tsx
+++ myapp/src/Routes/Home.tsx
@@ -10,6 +10,7 @@ import {
 } from "framer-motion";
 import {useState} from "react";
 import {useHistory, useRouteMatch} from "react-router-dom";
+import {theme} from "../theme";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -146,6 +147,31 @@ const BigMovie = styled(motion.div)`
   left: 0;
   right: 0;
   margin: 0 auto;
+  background-color: ${(props) => props.theme.black.lighter};
+  border-radius: 15px;
+  overflow: hidden;
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const offset = 6;
@@ -186,6 +212,13 @@ function Home() {
 
   //overlay를 눌렀을 경우 다시 /링크로 돌아가게
   const onOverlayClick = () => history.push("/");
+
+  //click한 영화의 아이디와 매치하여 이미지를 불러오기 위함
+  const clickedMovie =
+    bigMovieMatch?.params.movieId &&
+    data?.results.find(
+      (movie) => movie.id + "" === bigMovieMatch.params.movieId
+    );
 
   return (
     <Wrapper>
@@ -252,9 +285,24 @@ function Home() {
                 />
                 <BigMovie
                   // 스크롤 위치 상관없이 모달을 정위치 시키기 위함
-                  style={{top: scrollY.get() + 10}}
+                  style={{top: scrollY.get() + 100}}
                   layoutId={bigMovieMatch.params.movieId}
-                ></BigMovie>
+                >
+                  {clickedMovie && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                            clickedMovie.backdrop_path,
+                            "w500"
+                          )})`,
+                        }}
+                      />
+                      <BigTitle>{clickedMovie.title}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
+                </BigMovie>
               </>
             ) : null}
           </AnimatePresence>
